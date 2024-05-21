12a7ec363ce7cde4fef8bd5f00f5c9ba28c23df8
elaurensx <elaurensh@gmail.com>
2024-05-21 16:25:25 +0200

    keepalived-haproxy

---
+++ keepalived/build-lb1.sh
@@ -0,0 +1,1 @@
+docker build --no-cache --build-arg LB=lb1 -t ghcr.io/elaurensx/keepalived-lb1:1.0.0 .
---
+++ keepalived/build-lb2.sh
@@ -0,0 +1,1 @@
+docker build --no-cache --build-arg LB=lb2 -t ghcr.io/elaurensx/keepalived-lb2:1.0.0 .
